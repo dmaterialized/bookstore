458d5cdcc7e021c8a7fd6f62e4d8b609ef2b5d8a
D.M. Cook <dmaterialized@gmail.com>
2017-05-25 02:29:21 -0400

    comment for legibility

--- script1.py
+++ script1.py
@@ -2,6 +2,7 @@ from tkinter import *
 
 window=Tk()
 
+# l a b e l   s e t u p
 l1=Label(window,text="Title")
 l1.grid(row=0,column=0)
 
@@ -14,13 +15,15 @@ l3.grid(row=0,column=4)
 l4=Label(window,text="ISBN")
 l4.grid(row=0,column=6)
 
-
+#set up stringvar to store field values
 title_text=StringVar()
 author_text=StringVar()
 isbn_text=StringVar()
 year_text=StringVar()
 
+# e n t r y   f i e l d   s e t u p
 e1=Entry(window,textvariable=title_text)
+# textvariable means that the content of the Entry can be called later
 e1.grid(row=0,column=1)
 
 e2=Entry(window,textvariable=author_text)
@@ -32,6 +35,8 @@ e3.grid(row=0,column=5)
 e4=Entry(window,textvariable=isbn_text)
 e4.grid(row=0,column=7)
 
+
+# l i s t b o x   s e t u p
 list1=Listbox(window,height=7,width=140)
 # list1.grid(row=1,column=0)
 # need to add a span across more than one column (not just one grid square)
@@ -39,12 +44,19 @@ list1.grid(row=1,column=0,columnspan=4)
 # attach a scrollbar
 # tell the scrollbar about the list
 
+
+
+# scrollbar setup
 sb1=Scrollbar(window)
 sb1.grid(row=1,column=5)
 # attach the list to scrollbar, using configure and yscrollcommand.
 list1.configure(yscrollcommand=sb1.set)
 # let scrollbar know that the list is what is scrolling
 sb1.configure(command=list1.yview)
+
+
+
+
 
 # start everything
 window.mainloop()
