8252135ac8d5196fbbbe22430910eca249571869
David-Michael Cook <dmaterialized@gmail.com>
2017-06-01 00:45:08 -0400

    add documentation for sql connect commands

--- backend.py
+++ backend.py
@@ -10,6 +10,7 @@ def connect():
 
 
 def insert(title, author, year, isbn):
+    # reuse all connection standards ==============
     conn=sqlite3.connect("books.db")
     cur=conn.cursor()
     cur.execute("INSERT INTO booktable VALUES (NULL,?,?,?,?)",(title,author,year,isbn))
@@ -18,6 +19,7 @@ def insert(title, author, year, isbn):
 
 def view():
     conn=sqlite3.connect("books.db")
+    # reuse all connection standards ==============
     cur=conn.cursor()
     cur.execute("SELECT * from booktable")
     rows=cur.fetchall()
@@ -28,6 +30,7 @@ def view():
 
 def search(title="",author="",year="",isbn=""):
     conn=sqlite3.connect("books.db")
+    # reuse all connection standards ==============
     cur=conn.cursor()
     cur.execute("SELECT * from booktable WHERE title=? OR author=? or year=? OR isbn=?",(title,author,year,isbn))
     rows=cur.fetchall()
@@ -38,20 +41,29 @@ def search(title="",author="",year="",isbn=""):
 
 def delete(id):
     conn=sqlite3.connect("books.db")
+    # reuse all connection standards ==============
     cur=conn.cursor()
     cur.execute("DELETE FROM booktable WHERE id=?",(id,))
-    # rows=cur.fetchall()
+    # rows=cur.fetchall() 
+    #     isn't used here
     conn.commit()
     conn.close()
+    
 
 def update(id, title, author, year, isbn):
     conn=sqlite3.connect("books.db")
+    # reuse all connection standards ==============
     cur=conn.cursor()
     cur.execute("UPDATE booktable SET title=?,author=?,year=?,isbn=? WHERE id=?",(title,author,year,isbn, id))
     conn.commit()
     conn.close()
 
+def intro(text):
+    # other params include expression, sides!
+    if text==1:
+        print("initializing...")
 
+intro(1)
 
 connect()
 
@@ -64,9 +76,9 @@ insert("The Earth", "John Smith", 1918, 91283423)
 insert("The Waves", "John Tyler", 1933, 12387123)
 
 print(view())
-print(search(author="John Tablet"))
-update(1, "The Moon", "John Denver", 3022, 31923)
-print(view())
-update(1, "The Moon", "John Denver", 3022, 31923)
-print("And then...")
+# TESTS
+#print(search(author="John Tablet"))
+#update(1,"Every Little Thing","Mr So And So", 2017, 543431)
+#print(view())
+#print("And then...")
 print(view())
