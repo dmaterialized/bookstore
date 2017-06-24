f1fb96a57d981745bc829d915881218b763fa762
D.M. Cook <dmaterialized@gmail.com>
2017-06-24 02:00:20 -0400

    add self to backend scripts

--- backend.py
+++ backend.py
@@ -1,6 +1,6 @@
 import sqlite3
 
-# create a database first
+# create a database first (using OOP)
 class Database:
     # create blueprint of the object
     # then create object instances
@@ -12,52 +12,35 @@ class Database:
     def __init__(self,db):# this syntax is a constructor
 
     # __init__ is how python creates an initial call when class is instanced.
-        conn=sqlite3.connect(db)
-        cur=conn.cursor()
-        cur.execute("CREATE TABLE IF NOT EXISTS booktable (id INTEGER PRIMARY KEY, title text, author text, year integer, isbn integer)")
-        conn.commit()
-        conn.close()
+        self.conn=sqlite3.connect(db)
+        self.cur=self.conn.cursor() # TODO: HOW: how exactly does self.cur work?
+        self.cur.execute("CREATE TABLE IF NOT EXISTS booktable (id INTEGER PRIMARY KEY, title text, author text, year integer, isbn integer)")
+        self.conn.commit()
 
     def insert(self,title, author, year, isbn):
-        conn=sqlite3.connect("books.db")
-        cur=conn.cursor()
-        cur.execute("INSERT INTO booktable VALUES (NULL,?,?,?,?)",(title,author,year,isbn))
-        conn.commit()
-        conn.close()
+        self.cur.execute("INSERT INTO booktable VALUES (NULL,?,?,?,?)",(title,author,year,isbn))
+        self.conn.commit()
 
     def view(self):
-        # TODO: doesn't work with (db)
-        conn=sqlite3.connect("books.db")
-        cur=conn.cursor()
-        cur.execute("SELECT * from booktable")
-        rows=cur.fetchall()
-        conn.commit()
-        conn.close()
+        self.cur.execute("SELECT * from booktable")
+        rows=self.cur.fetchall()
+        self.conn.commit()
         return rows
 
     def search(self,title="",author="",year="",isbn=""):
-        conn=sqlite3.connect("books.db")
-        cur=conn.cursor()
-        cur.execute("SELECT * from booktable WHERE title=? OR author=? or year=? OR isbn=?",(title,author,year,isbn))
-        rows=cur.fetchall()
-        conn.commit()
-        conn.close()
+        self.cur.execute("SELECT * from booktable WHERE title=? OR author=? or year=? OR isbn=?",(title,author,year,isbn))
+        rows=self.cur.fetchall()
+        self.conn.commit()
         return rows
 
     def delete(self,id):
-        conn=sqlite3.connect("books.db")
-        cur=conn.cursor()
-        cur.execute("DELETE FROM booktable WHERE id=?",(id,))
+        self.cur.execute("DELETE FROM booktable WHERE id=?",(id,))
         # rows=cur.fetchall()
-        conn.commit()
-        conn.close()
+        self.conn.commit()
 
     def update(self,id, title, author, year, isbn):
-        conn=sqlite3.connect("books.db")
-        cur=conn.cursor()
-        cur.execute("UPDATE booktable SET title=?,author=?,year=?,isbn=? WHERE id=?",(title,author,year,isbn, id))
-        conn.commit()
-        conn.close()
+        self.cur.execute("UPDATE booktable SET title=?,author=?,year=?,isbn=? WHERE id=?",(title,author,year,isbn, id))
+        self.conn.commit()
 
 
 print("Backend initialized.")
